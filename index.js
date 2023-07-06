56ddd4df85c93d9691f08b6fd89f6155cd1929b5
Harshit44 <harshitbihani44@gmail.com>
2023-07-06 18:21:32 +0530

    Touch added

--- index.js
+++ index.js
@@ -10,31 +10,56 @@ document.querySelector(" .w").addEventListener("click", function (){
 document.querySelector(" .a").addEventListener("click", function (){
     var tom2 = new Audio("sounds/tom-2.mp3");
     tom2.play();
+    document.querySelector(".a").classList.add("pressed")
+            setTimeout(function(){
+                document.querySelector(".a").classList.remove("pressed")
+            }, 100)
+    
     console.log("Ckicked");
 });
 document.querySelector(" .s").addEventListener("click", function (){
     var tom3 = new Audio("sounds/tom-3.mp3");
     tom3.play();
+    document.querySelector(".s").classList.add("pressed")
+            setTimeout(function(){
+                document.querySelector(".s").classList.remove("pressed")
+            }, 100)
     console.log("Ckicked");
 });
 document.querySelector(" .d").addEventListener("click", function (){
     var tom4 = new Audio("sounds/tom-4.mp3");
     tom4.play();
+    document.querySelector(".d").classList.add("pressed")
+            setTimeout(function(){
+                document.querySelector(".d").classList.remove("pressed")
+            }, 100)
     console.log("Ckicked");
 });
 document.querySelector(" .j").addEventListener("click", function (){
     var snare = new Audio("sounds/snare.mp3");
     snare.play();
+    document.querySelector(".j").classList.add("pressed")
+            setTimeout(function(){
+                document.querySelector(".j").classList.remove("pressed")
+            }, 100)
     console.log("Ckicked");
 });
 document.querySelector(" .k").addEventListener("click", function (){
     var kick = new Audio("sounds/kick-bass.mp3");
     kick.play();
+    document.querySelector(".k").classList.add("pressed")
+            setTimeout(function(){
+                document.querySelector(".k").classList.remove("pressed")
+            }, 100)
     console.log("Ckicked");
 });
 document.querySelector(" .l").addEventListener("click", function (){
     var crash = new Audio("sounds/crash.mp3");
     crash.play();
+    document.querySelector(".l").classList.add("pressed")
+            setTimeout(function(){
+                document.querySelector(".l").classList.remove("pressed")
+            }, 100)
     console.log("Ckicked");
 });
 
