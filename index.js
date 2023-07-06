cbdbf6200ec01a5092cf4ef9536d119bf0a54e99
Harshit44 <harshitbihani44@gmail.com>
2023-07-06 18:06:34 +0530

    animations added

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ document.querySelector(" .w").addEventListener("click", function (){
     var tom1 = new Audio("sounds/tom-1.mp3");
     tom1.play();
     console.log("Ckicked");
+    document.querySelector(".w").classList.add("pressed")
+            setTimeout(function(){
+                document.querySelector(".w").classList.remove("pressed")
+            }, 100)
 });
 document.querySelector(" .a").addEventListener("click", function (){
     var tom2 = new Audio("sounds/tom-2.mp3");
@@ -36,35 +40,64 @@ document.querySelector(" .l").addEventListener("click", function (){
 
 
 document.addEventListener("keypress", function(event){
+    console.log()
     keyvalue = event.key;
     switch (keyvalue) {
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();  
+            tom1.play(); 
+            document.querySelector(".w").classList.add("pressed")
+            setTimeout(function(){
+                document.querySelector(".w").classList.remove("pressed")
+            }, 100) 
             break;
         case "a":
             var tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play();  
+            tom2.play();
+            document.querySelector(".a").classList.add("pressed")
+            setTimeout(function(){
+                document.querySelector(".a").classList.remove("pressed")
+            }, 100)  
             break;
         case "s":
             var tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();  
+            tom3.play(); 
+            document.querySelector(".s").classList.add("pressed")
+            setTimeout(function(){
+                document.querySelector(".s").classList.remove("pressed")
+            }, 100) 
             break;
         case "d":
             var tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();  
+            tom4.play(); 
+            document.querySelector(".d").classList.add("pressed")
+            setTimeout(function(){
+                document.querySelector(".d").classList.remove("pressed")
+            }, 100) 
             break;
         case "j":
             var snare = new Audio("sounds/snare.mp3");
-            snare.play();  
+            snare.play(); 
+            document.querySelector(".j").classList.add("pressed")
+            setTimeout(function(){
+                document.querySelector(".j").classList.remove("pressed")
+            }, 100) 
             break;
         case "k":
             var kick = new Audio("sounds/kick-bass.mp3");
             kick.play();  
+            document.querySelector(".k").classList.add("pressed")
+            setTimeout(function(){
+                document.querySelector(".k").classList.remove("pressed")
+            }, 100)
             break;
         case "l":
             var crash = new Audio("sounds/crash.mp3");
-            crash.play();  
+            crash.play(); 
+            document.querySelector(".l").classList.add("pressed")
+            setTimeout(function(){
+                document.querySelector(".l").classList.remove("pressed")
+            }, 100) 
             break;
         default:
             break;
